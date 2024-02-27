0dc4a7438532797f122e774bcd7ebd87eada428e
RT <ryan@pounce.cr>
2024-02-27 11:03:39 +1100

    Launced to Heroku

--- routes/activity.js
+++ routes/activity.js
@@ -27,6 +27,9 @@ exports.execute = async (req, res) => {
         values: {
           Event: data.inArguments[0].DropdownOptions,
           Text: data.inArguments[0].Text,
+          Payload: data.inArguments[0].Journey,
+          Payload: data.inArguments[0].Version,
+          Payload: data.inArguments[0].Path,
           Payload: data.inArguments[0].Payload,
         },
       },
